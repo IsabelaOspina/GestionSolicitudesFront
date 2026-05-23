30c85ae0d662c0f61b3da584cf030ba06d8f1655
nikoll <nikolls.ocampog@uqvirtual.edu.co>
2026-05-23 12:27:54 -0500

    Feat:component

---
+++ src/app/Components/login/login.component.ts
@@ -0,0 +1,71 @@
+import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+
+import { UsuarioService } from '../../Service/usuario.service';
+
+@Component({
+  selector: 'app-login',
+  standalone: true,
+  imports: [FormsModule, CommonModule],
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css']
+})
+export class LoginComponent {
+
+  correoElectronico = '';
+  password = '';
+
+  error = '';
+
+  constructor(
+    private usuarioService: UsuarioService,
+    private router: Router
+  ) {}
+
+  login(): void {
+
+    const dto = {
+      correoElectronico: this.correoElectronico,
+      password: this.password
+    };
+
+    this.usuarioService.login(dto).subscribe({
+
+      next: (response) => {
+
+        localStorage.setItem('token', response.token);
+
+        const payload = JSON.parse(
+          atob(response.token.split('.')[1])
+        );
+
+        const rol = payload.role;
+
+        localStorage.setItem('rol', rol);
+
+        if (rol === 'ADMINISTRADOR') {
+          this.router.navigate(['/admin']);
+        }
+
+        else if (rol === 'ADMINISTRATIVO') {
+          this.router.navigate(['/administrativo']);
+        }
+
+        else if (
+          rol === 'ESTUDIANTE' ||
+          rol === 'DOCENTE'
+        ) {
+          this.router.navigate(['/solicitudes']);
+        }
+
+      },
+
+      error: () => {
+        this.error = 'Credenciales incorrectas';
+      }
+    });
+  }
+}
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,25 @@
 import { Routes } from '@angular/router';
+import { LoginComponent } from './Components/login/login.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+
+  {
+    path: '',
+    component: LoginComponent
+  },
+
+  {
+    path: 'admin',
+    component: LoginComponent
+  },
+
+  {
+    path: 'administrativo',
+    component: LoginComponent
+  },
+
+  {
+    path: 'solicitudes',
+    component: LoginComponent
+  }
+];
